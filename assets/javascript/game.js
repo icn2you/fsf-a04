ff951eafe0dfcdc048c61791c2a8c56888cf98a5
Christopher Zenner <christopher.zenner@gmail.com>
2020-03-06 22:50:03 -0800

    feat: Add random number generation
    - Create generator methods for game's random number (the number user
      must match with piece values) and random numbers for each game piece.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,13 +13,15 @@ stones.
 class CollectorGame {
   name = "Collector";
   instructions = "Click one of the icons to get started.";
-  collection = [];
+  collection = {};
   score = 0;
   losses = 0;
   wins = 0;
   randomNum;
   #min = 19;
   #max = 120;
+  #pieceMin = 1;
+  #pieceMax = 12;
 
   constructor(name, instructions, collection) {
     this.name = name;
@@ -34,13 +36,35 @@ class CollectorGame {
   generateRandomNum() {
     this.randomNum = Math.floor(Math.random() * (this.#max - this.#min + 1)) + this.#min;
   }
+
+  generatePieceVals() {
+    for (var key in this.collection) {
+      this.collection[key] = Math.floor(Math.random() * (this.#pieceMax - this.#pieceMin + 1)) + this.#pieceMin;
+    }
+
+    // DEBUG
+    for (var key in this.collection) {
+      console.log(key + " is assigned a value of " + this.collection[key] + ".");
+    }
+  }
 }
 
 // Execute script once page is fully loaded
 $(document).ready(function() {
-  let GemCollector = new CollectorGame("Gem Collector", "...", []);
-  console.log(GemCollector);
+  let gemCollector = new CollectorGame("Gem Collector", 
+    "...", 
+    {"amethyst": 0, "citrine": 0, "emerald": 0, "ruby": 0});
+  // DEBUG
+  // console.log(gemCollector);
 
-  GemCollector.generateRandomNum();
-  console.log(GemCollector.getRandomNum());
+  gemCollector.generateRandomNum();
+
+  $("img").click(function(event) {
+    // DEBUG
+    console.log(event.target.id + " clicked!");
+
+    gemCollector.generatePieceVals();
+
+    $("#random-number").text(gemCollector.getRandomNum());
+  });
 });
